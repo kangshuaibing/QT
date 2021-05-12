4085c5a60e35894e23f15a0c437be9fe0b13c176
kangWork <1326799350@qq.com>
2021-05-12 16:25:27 +0800

    OPENCV保存视频

---
+++ OpenCVSave/mainwindow.cpp
@@ -0,0 +1,75 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    time = new QTimer(this);
+    time->start(10);
+
+    connect(time, SIGNAL(timeout()), this, SLOT(readFrame()));
+
+    counter = 0;
+
+    cap.open(0);
+    write.open("test.avi", 1, 25.0, Size(640, 480), true); //保存在当前目录，名为test的AVI视频
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//读取摄像头视频并保存
+void MainWindow::readFrame()
+{
+   cap >> frame;
+   counter++;
+   if(frame.empty())
+   {
+        qDebug()<<"frame is empty";
+        cap.release();
+   }
+
+   if (counter <= 100)  //只保存100帧
+   {
+       qDebug() << "counter = " << counter;
+       write.write(frame);  //保存视频帧
+   }
+   ui->label->setPixmap(QPixmap::fromImage(mat2QImage(frame)));
+}
+
+//Mat to QImage
+QImage MainWindow::mat2QImage(cv::Mat cvImg)
+{
+    QImage qImg;
+
+    if (cvImg.channels() == 3)                             //3 channels color image
+    {
+        cv::cvtColor(cvImg, cvImg, CV_BGR2RGB);
+        qImg = QImage((const unsigned char*)(cvImg.data),
+                      cvImg.cols, cvImg.rows,
+                      cvImg.cols*cvImg.channels(),
+                      QImage::Format_RGB888);
+    }
+
+    else if (cvImg.channels() == 1)                    //grayscale image
+    {
+        qImg = QImage((const unsigned char*)(cvImg.data),
+                      cvImg.cols, cvImg.rows,
+                      cvImg.cols*cvImg.channels(),
+                      QImage::Format_Indexed8);
+    }
+
+    else
+    {
+        qImg = QImage((const unsigned char*)(cvImg.data),
+                      cvImg.cols, cvImg.rows,
+                      cvImg.cols*cvImg.channels(),
+                      QImage::Format_RGB888);
+    }
+    return qImg;
+}
---
+++ OpenCVSave/mainwindow.h
@@ -0,0 +1,42 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#include <QDebug>
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    QImage mat2QImage(cv::Mat cvImg);
+
+private slots:
+    void readFrame();
+
+private:
+    Ui::MainWindow *ui;
+
+    QTimer *time;
+
+    Mat frame;
+
+    VideoCapture cap;
+    VideoWriter write;
+
+    int counter;  //记录帧数
+};
+
+#endif // MAINWINDOW_H
